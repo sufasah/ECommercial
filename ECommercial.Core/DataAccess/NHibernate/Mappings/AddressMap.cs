8f3b7daa6b8637363ddcc4309131e4503c3ffadb
sufasah <sufasah@gmail.com>
2020-10-13 11:39:44 +0300

    solved problems occured

--- ECommercial.Core/DataAccess/NHibernate/Mappings/AddressMap.cs
+++ ECommercial.Core/DataAccess/NHibernate/Mappings/AddressMap.cs
@@ -1,4 +1,3 @@
-using ECommercial.Entites.concrete;
 namespace ECommercial.Core.DataAccess.NHibernate.Mappings
 {
     public class AddressMap
--- ECommercial.DataAccess/Abstract/IUserProductWillBeOrderedDal.cs
+++ ECommercial.DataAccess/Abstract/IUserProductWillBeOrderedDal.cs
@@ -1,5 +1,5 @@
 using ECommercial.Entites.concrete;
-
+using ECommercial.Core.DataAccess;
 namespace ECommercial.DataAccess.Abstract
 {
     public interface IUserProductWillBeOrderedDal: Core.DataAccess.IEntityRepository<UserProductWillBeOrdered>
