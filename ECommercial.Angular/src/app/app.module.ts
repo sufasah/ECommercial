d46542a54c64ffe958670dffa08b667b4f1ecd6e
sufasah <sufasah@gmail.com>
2020-11-06 21:45:04 +0300

    Introduced to angular

--- ECommercial.Angular/src/app/app.module.ts
+++ ECommercial.Angular/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './modules/home-page/home-page.component';
+import { HeaderComponent } from './modules/home-page/components/header/header.component';
+import { CartComponent } from './modules/home-page/components/cart/cart.component';
+import { CategoryNavComponent } from './components/home-page/components/category-nav/category-nav.component';
+import { SliderComponent } from './modules/home-page/components/slider/slider.component';
+import { BannerComponent } from './shared/components/banner/banner.component';
+import { ExploreComponent } from './modules/home-page/components/explore/explore.component';
+import { FooterComponent } from './modules/home-page/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    CartComponent,
+    CategoryNavComponent,
+    SliderComponent,
+    BannerComponent,
+    ExploreComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ ECommercial.Angular/src/app/modules/home-page/components/category-nav/category-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-nav',
+  templateUrl: './category-nav.component.html',
+  styleUrls: ['./category-nav.component.css']
+})
+export class CategoryNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
