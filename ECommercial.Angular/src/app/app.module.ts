d88aed1353a9c438b26210d7580927da4aeaa1ea
sufasah <sufasah@gmail.com>
2020-11-07 10:54:44 +0300

    angular homepage created. project structure has been formed

--- ECommercial.Angular/src/app/app.module.ts
+++ ECommercial.Angular/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/*
+  Home Page Components
+ */
 import { HomePageComponent } from './modules/home-page/home-page.component';
-import { HeaderComponent } from './modules/home-page/components/header/header.component';
+import { HeaderComponent } from './core/header/header.component';
 import { CartComponent } from './modules/home-page/components/cart/cart.component';
-import { CategoryNavComponent } from './components/home-page/components/category-nav/category-nav.component';
-import { SliderComponent } from './modules/home-page/components/slider/slider.component';
-import { BannerComponent } from './shared/components/banner/banner.component';
+import { CategoryNavComponent } from './modules/home-page/components/category-nav/category-nav.component';
 import { ExploreComponent } from './modules/home-page/components/explore/explore.component';
-import { FooterComponent } from './modules/home-page/components/footer/footer.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { SliderComponent } from './modules/home-page/components/slider/slider.component';
+/*
+End of Home Page Components
+ */
 
 @NgModule({
   declarations: [
@@ -19,10 +25,9 @@ import { FooterComponent } from './modules/home-page/components/footer/footer.co
     HeaderComponent,
     CartComponent,
     CategoryNavComponent,
-    SliderComponent,
-    BannerComponent,
     ExploreComponent,
     FooterComponent,
+    SliderComponent,
   ],
   imports: [
     BrowserModule,
