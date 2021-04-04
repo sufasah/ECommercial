8f83ec4ddb0273949739bd60a84c58f911fbb183
sufasah <sufasah@gmail.com>
2021-04-04 09:34:05 +0300

    event and data bindings added

--- ECommercial.WebApi/ClientApp/src/app/modules/login-page/login-page.component.ts
+++ ECommercial.WebApi/ClientApp/src/app/modules/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  rememberMe=false;
+  email="";
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+  rememberMeChanged(){
+    this.rememberMe=!this.rememberMe;
+  }
+
+  showPasswordPart(){
+    let loginFirst = $(".login-first");
+    let loginSecond = $(".login-second");
+
+    loginFirst.addClass("d-none");
+    loginSecond.removeClass("d-none");
+  }
+
+  showEmailPart(){
+    let loginFirst = $(".login-first");
+    let loginSecond = $(".login-second");
+
+    loginFirst.removeClass("d-none");
+    loginSecond.addClass("d-none");
+  }
+
 
 }
